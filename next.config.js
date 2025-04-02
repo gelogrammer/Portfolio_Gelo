672c739ed70ca26312c60c92a9e9fbf0c3f6692e
Angelo John S. Calleja <angelojohnse.calleja@bicol-u.edu.ph>
2025-04-02 21:07:50 +0800

    Configure for Cloudflare Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true, // Required for static exports
+  },
+  // Indicate that the site will be deployed on Cloudflare Pages
+  experimental: {
+    isrMemoryCacheSize: 0, // Disable ISR caching
+  },
 }
 
 module.exports = nextConfig 
